72b4cc61e587c1b124a5447a4aeb12b5c754455c
KELVIN W <c11200039@john.petra.ac.id>
2023-06-28 15:03:01 +0700

    FINAL

--- pendaftaran_turnamen (1).sql
+++ pendaftaran_turnamen (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2023 at 10:59 PM
+-- Generation Time: Jun 28, 2023 at 10:01 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -38,6 +38,7 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`username`, `password`) VALUES
 ('Albert', '412'),
+('Bobby', 'Bola'),
 ('Derick', '456'),
 ('Kelvin', '123');
 
@@ -54,6 +55,15 @@ CREATE TABLE `bracket_individu` (
   `komentar` varchar(45) DEFAULT NULL,
   `idbracketIndiv` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `bracket_individu`
+--
+
+INSERT INTO `bracket_individu` (`idpeserta`, `idpeserta1`, `date`, `komentar`, `idbracketIndiv`) VALUES
+(1, 2, '2022-06-28', NULL, 2),
+(2, 1, '2023-06-28', NULL, 3),
+(2, 1, '2023-06-28', NULL, 4);
 
 -- --------------------------------------------------------
 
@@ -74,9 +84,10 @@ CREATE TABLE `bracket_team` (
 --
 
 INSERT INTO `bracket_team` (`team_idteam`, `team_idteam1`, `date`, `komentar`, `idBracketTeam`) VALUES
-(1, 1, '2022-01-11', NULL, 4),
+(1, 2, '2022-01-11', NULL, 4),
 (1, 2, '2023-06-27', NULL, 5),
-(1, 2, '2023-06-27', NULL, 6);
+(1, 2, '2023-06-27', NULL, 6),
+(1, 2, '2022-06-28', NULL, 8);
 
 -- --------------------------------------------------------
 
@@ -99,7 +110,9 @@ INSERT INTO `game_individu` (`idgame`, `nama_game`) VALUES
 (3, 'Survival.io'),
 (4, 'Genshin Impact'),
 (5, 'NBA 2k20'),
-(6, 'God Of War');
+(6, 'God Of War'),
+(7, 'DOTA'),
+(29, 'Qubika');
 
 -- --------------------------------------------------------
 
@@ -121,7 +134,8 @@ INSERT INTO `game_kelompok` (`idgame`, `nama_game`) VALUES
 (2, 'Valorant'),
 (3, 'Mobile Legends'),
 (4, 'Overwatch'),
-(5, 'Fifa');
+(5, 'Fifa'),
+(6, 'Homura');
 
 -- --------------------------------------------------------
 
@@ -150,10 +164,11 @@ CREATE TABLE `peserta_individu` (
 --
 
 INSERT INTO `peserta_individu` (`idpeserta`, `nama`, `alamat`, `tanggal_lahir`, `no_handphone`, `nrp`, `program_studi`, `angkatan`, `foto_formal`, `win`, `lose`, `game_individu_idgame`, `status`) VALUES
-(1, 'Justin', 'Gianyar V C6/27', '2002-04-28', '08101010101', 'c11200012', 'Teknik Elektro', '2020', NULL, 0, 0, 1, 1),
-(2, 'Juan F', 'Sidoarjo 01', '2002-05-15', '0820202202', 'c11200013', 'Teknik Elektro', '2020', NULL, 0, 0, 1, 1),
-(3, 'Jeremy', 'Tanjung Priok 02', '2001-09-09', '08103030303', 'c11200001', 'Teknik Elektro', '2021', NULL, 0, 0, 3, 2),
-(4, 'Anthony', 'Tanjung  03', '2001-09-10', '08040404040', 'c11200025', 'Teknik Elektro', '2022', NULL, 0, 0, 2, 0);
+(1, 'Justin', 'Gianyar V C6/27', '2002-04-28', '08101010101', 'c11200012', 'Teknik Elektro', '2020', NULL, 7, 1, 1, 1),
+(2, 'Juan F', 'Sidoarjo 01', '2002-05-15', '0820202202', 'c11200013', 'Teknik Elektro', '2020', NULL, 3, 0, 1, 1),
+(3, 'Jeremy', 'Tanjung Priok 02', '2001-09-09', '08103030303', 'c11200001', 'Teknik Elektro', '2021', NULL, 0, 0, 1, 2),
+(4, 'Anthony', 'Tanjung  03', '2001-09-10', '08040404040', 'c11200025', 'Teknik Elektro', '2022', NULL, 0, 0, 1, 0),
+(5, 'Gorila', 'Gianyar C6/27', '2002-04-26', '081929292', 'c11200050', 'Teknik Elektro', '2020', NULL, 0, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -187,7 +202,8 @@ INSERT INTO `peserta_kelompok` (`idpeserta`, `nama`, `alamat`, `tanggal_lahir`, 
 (6, 'Julius', 'Dangsir 26', '2002-07-19', '087802301929', 'c11200020', 'DKV', '2020', NULL, 3),
 (7, 'Albert', 'Kencana 27', '2002-09-13', '087802301929', 'c11200035', 'DFT', '2020', NULL, 3),
 (8, 'Derick', 'Dangsir 55', '2002-11-01', '087802301929', 'c11200036', 'Teknik Informatika', '2020', NULL, 4),
-(9, 'Yegia', 'Kutisari 08', '2002-03-06', '087802301929', 'c11200037', 'Mesin', '2020', NULL, 4);
+(9, 'Yegia', 'Kutisari 08', '2002-03-06', '087802301929', 'c11200037', 'Mesin', '2020', NULL, 4),
+(10, 'Sakura', 'Jepang 27', '2004-05-26', '081929292', 'c11200057', 'DSA', '2021', NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -201,18 +217,20 @@ CREATE TABLE `team` (
   `win` int(11) DEFAULT NULL,
   `lose` int(11) DEFAULT NULL,
   `game_kelompok_idgame` int(11) NOT NULL,
-  `status` int(11) DEFAULT NULL
+  `status` int(11) DEFAULT NULL,
+  `password` varchar(455) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `team`
 --
 
-INSERT INTO `team` (`idteam`, `nama_team`, `win`, `lose`, `game_kelompok_idgame`, `status`) VALUES
-(1, 'GABIMARU', 0, 0, 1, 3),
-(2, 'KECAMBAH', 0, 0, 2, 3),
-(3, 'ULET', 0, 0, 3, 2),
-(4, 'SEMANGAT', 0, 0, 4, 0);
+INSERT INTO `team` (`idteam`, `nama_team`, `win`, `lose`, `game_kelompok_idgame`, `status`, `password`) VALUES
+(1, 'GABIMARU', 3, 0, 1, 1, '123'),
+(2, 'KECAMBAH', 0, 0, 1, 1, '123'),
+(3, 'ULET', 0, 0, 1, 2, '123'),
+(4, 'SEMANGAT', 0, 0, 1, 0, '123'),
+(5, 'Nihonjin', 0, 0, 2, 0, 'Nippon11');
 
 --
 -- Indexes for dumped tables
@@ -281,43 +299,43 @@ ALTER TABLE `team`
 -- AUTO_INCREMENT for table `bracket_individu`
 --
 ALTER TABLE `bracket_individu`
-  MODIFY `idbracketIndiv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idbracketIndiv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `bracket_team`
 --
 ALTER TABLE `bracket_team`
-  MODIFY `idBracketTeam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idBracketTeam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `game_individu`
 --
 ALTER TABLE `game_individu`
-  MODIFY `idgame` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `idgame` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `game_kelompok`
 --
 ALTER TABLE `game_kelompok`
-  MODIFY `idgame` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `idgame` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `peserta_individu`
 --
 ALTER TABLE `peserta_individu`
-  MODIFY `idpeserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idpeserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `peserta_kelompok`
 --
 ALTER TABLE `peserta_kelompok`
-  MODIFY `idpeserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `idpeserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `team`
 --
 ALTER TABLE `team`
-  MODIFY `idteam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idteam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
